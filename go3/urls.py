a72edd0648ebfbc52d25215aa1d8f2ed12ef1e7b
aaronoppenheimer <aoppenheimer@gmail.com>
2019-12-19 21:55:22 -0500

    adding auth system

--- go3/urls.py
+++ go3/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('band/', include('band.urls')),
     path('member/', include('member.urls')),
     path('admin/', admin.site.urls),
