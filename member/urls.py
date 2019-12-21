64eb05dd725db2daf37c0f2bc3c9363363eb4105
aaronoppenheimer <aoppenheimer@gmail.com>
2019-12-21 11:48:10 -0500

    wip

--- member/urls.py
+++ member/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
--- member/views.py
+++ member/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from .models import Member
+from django.views import generic
 
 def index(request):
     return HttpResponse("Hello, world. You're at the member index.")
+
+class DetailView(generic.DetailView):
+    model = Member
+    template_name = 'member/detail.html'
