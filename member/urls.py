80a38087889fedb683ae9b2e086f2a1a1dae7d0d
aaronoppenheimer <aoppenheimer@gmail.com>
2020-01-02 19:43:41 -0500

    better url formatting

--- member/urls.py
+++ member/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='member-detail'),
-    path('update/<int:pk>/', views.UpdateView.as_view(), name='member-update'),
-    path('preferences/update/<int:pk>/', views.PreferencesUpdateView.as_view(), name='member-prefs-update'),
+    path('<int:pk>/update', views.UpdateView.as_view(), name='member-update'),
+    path('<int:pk>/preferences/update/', views.PreferencesUpdateView.as_view(), name='member-prefs-update'),
 ]
