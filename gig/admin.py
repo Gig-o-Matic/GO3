6faedbebfcd2c6d918c532616954cfe3c380fd55
aaronoppenheimer <aoppenheimer@gmail.com>
2020-01-05 18:06:50 -0500

    working on gig model

--- gig/admin.py
+++ gig/admin.py
@@ -16,5 +16,6 @@
 """
 
 from django.contrib import admin
+from .models import Gig
 
-# Register your models here.
+admin.site.register(Gig)
--- gig/models.py
+++ gig/models.py
@@ -15,5 +15,70 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from django.db import models
+from member.models import Member
 
-# Create your models here.
+class Gig(models.Model):
+    title = models.CharField(max_length=200)
+
+    # todo when a member leaves the band must set their contact_gigs to no contact. Nolo Contacto!
+    contact = models.ForeignKey(Member, null=True, related_name="contact_gigs", on_delete=models.SET_NULL)
+    details = models.TextField(null=True, blank=True)
+    setlist = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    date = models.DateField(null=True, blank=True)
+    enddate = models.DateField(null=True, blank=True)
+
+    calltime = models.TimeField(null=True, blank=True)
+    settime = models.TimeField(null=True, blank=True)
+    endtime = models.TimeField(null=True, blank=True)
+
+    address = models.TextField(null=True, blank=True)
+    dress = models.TextField(null=True, blank=True)
+    paid = models.TextField(null=True, blank=True)
+    postgig = models.TextField(null=True, blank=True)
+
+    leader = models.ForeignKey(Member, null=True, related_name="leader_gigs", on_delete=models.SET_NULL)
+
+    # todo manage these
+    # trueenddate = ndb.ComputedProperty(lambda self: self.enddate if self.enddate else self.date)
+    # sorttime = ndb.IntegerProperty( default=None )
+
+    class StatusOptions(models.IntegerChoices):
+            UNKNOWN = 0
+            CONFIRMED = 1
+            CANCELLED = 2
+            ASKING = 3
+    status = models.IntegerField(choices=StatusOptions.choices, default=StatusOptions.UNKNOWN)
+
+    @property
+    def is_canceled():
+        self.status=StatusOptions.CANCELLED
+
+    @property
+    def is_confirmed():
+        self.status=StatusOptions.CONFIRMED
+
+    # todo archive
+    # archive_id = ndb.TextProperty( default=None )
+    # is_archived = ndb.ComputedProperty(lambda self: self.archive_id is not None)
+
+    is_private = models.BooleanField(default=False )    
+
+    # todo what's this?
+    # comment_id = ndb.TextProperty( default = None)
+
+    creator = models.ForeignKey(Member, null=True, related_name="creator_gigs", on_delete=models.SET_NULL)
+
+    invite_occasionals = models.BooleanField(default=True)
+    was_reminded = models.BooleanField(default=False)
+    hide_from_calendar = models.BooleanField(default=False)
+    default_to_attending = models.BooleanField( default=False )
+
+    # rss_description = ndb.TextProperty( default=None )
+    trashed_date = models.DateTimeField( blank=True, null=True )
+
+    @property
+    def is_in_trash():
+        return self.trashed_date is not None
