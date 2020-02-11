41103cf0bcaafa032bbd70b641ecab92b66dc0f3
aaronoppenheimer <aoppenheimer@gmail.com>
2020-02-10 21:39:23 -0500

    first cut at adding translations

--- go3/settings.py
+++ go3/settings.py
@@ -152,7 +152,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+	
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
--- go3/urls.py
+++ go3/urls.py
@@ -32,12 +32,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+urlpatterns = []
+
+urlpatterns += i18n_patterns(
     path('', include('agenda.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('band/', include('band.urls')),
     path('member/', include('member.urls')),
     path('gig/', include('gig.urls')),
     path('admin/', admin.site.urls),
-]
+)
