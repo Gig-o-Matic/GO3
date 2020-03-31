39441f690a884a4dcb7c4c2fa6e80179b77aa76b
aaronoppenheimer <aoppenheimer@gmail.com>
2020-03-30 21:42:54 -0400

    removed commented-out code

--- member/signals.py
+++ member/signals.py
@@ -25,7 +25,3 @@ def handle_user_preferences(sender, instance, created, **kwargs):
         MemberPreferences.objects.create(member=instance)
     else:
         instance.preferences.save()
-
-# @receiver(post_save, sender=Member)
-# def save_user_preferences(sender, instance, **kwargs):
-#     instance.preferences.save()
