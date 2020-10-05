8a2cff337a718002e700f3ecda89ea2707d48679
aaronoppenheimer <aoppenheimer@gmail.com>
2020-10-04 21:57:18 -0400

    added task for archiving old gigs

--- gig/tasks.py
+++ gig/tasks.py
@@ -17,16 +17,27 @@
 from gig.models import Gig
 from django.utils import timezone
 from datetime import timedelta
+from django.db.models import Q
 
 def delete_old_trashed_gigs():
     """
     Deletes gigs that were tashed more than 30 days ago
     """
-
     cutoff = timezone.now() - timedelta(days=30)
     old_trash = Gig.objects.filter(trashed_date__lt=cutoff)
     num = old_trash.count()
     old_trash.delete()
     return f'deleted {num} old trashed gigs'
     
-    
+def archive_old_gigs():
+    """
+    Archived gigs that have end dates that have passed
+    """
+    over_gigs = Gig.objects.filter(enddate__lt=timezone.now(), is_archived=False)
+    now = timezone.now()
+    over_gigs = Gig.objects.filter(Q(enddate__lt=now) | (Q(date__lt=now) & Q(enddate=None)),
+                                    is_archived=False)
+    num = over_gigs.count()
+    over_gigs.update(is_archived=True)
+    return f'archived {num} gigs'
+
