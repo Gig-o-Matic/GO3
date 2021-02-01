5b67eacf82fd4aee22f71fc086171e53ea23eaae
Mark <17260591+MarkKleinhaus@users.noreply.github.com>
2021-02-01 08:55:58 -0500

    updated fakedata fixture to automatically output testdata and overwrite if exists

--- fixtures/makefakedata.py
+++ fixtures/makefakedata.py
@@ -3,76 +3,89 @@ import random
 import json
 import datetime
 
-names=[]
-with urllib.request.urlopen('http://names.drycodes.com/100?nameOptions=boy_names&separator=space&format=json') as f:
-    names=json.loads((f.read().decode('utf-8')))
-with urllib.request.urlopen('http://names.drycodes.com/100?nameOptions=girl_names&separator=space&format=json') as f:
-    names += json.loads((f.read().decode('utf-8')))
+names = []
+with urllib.request.urlopen(
+    "http://names.drycodes.com/100?nameOptions=boy_names&separator=space&format=json"
+) as f:
+    names = json.loads((f.read().decode("utf-8")))
+with urllib.request.urlopen(
+    "http://names.drycodes.com/100?nameOptions=girl_names&separator=space&format=json"
+) as f:
+    names += json.loads((f.read().decode("utf-8")))
 
 # make a bunch of people
-people=[]
-for i in range(0,200):
+people = []
+for i in range(0, 200):
     n = names[i]
-    people.append({
-        "model": "member.member",
-        "pk":100+i,
-        "fields" : {
-            "email": f'{n.replace(" ","")}@foo.com',
-            "username": n,
+    people.append(
+        {
+            "model": "member.member",
+            "pk": 100 + i,
+            "fields": {
+                "email": f'{n.replace(" ","")}@foo.com',
+                "username": n,
+            },
         }
-    })
+    )
 
 
+band_names = []
+b_prefix = ["The ", ""]
+with urllib.request.urlopen(
+    "http://names.drycodes.com/25?nameOptions=funnyWords&separator=space&format=json"
+) as f:
+    b_main = json.loads(f.read().decode("utf-8"))
+b_type = [" Band", " Brass Band", " Barching Band", ""]
 
-band_names=[]
-b_prefix=['The ','']
-with urllib.request.urlopen('http://names.drycodes.com/25?nameOptions=funnyWords&separator=space&format=json') as f:
-    b_main= json.loads(f.read().decode('utf-8'))
-b_type=[' Band',' Brass Band',' Barching Band','']
-
-with urllib.request.urlopen('http://names.drycodes.com/25?nameOptions=cities&separator=space&format=json') as f:
-    cities = json.loads(f.read().decode('utf-8'))
+with urllib.request.urlopen(
+    "http://names.drycodes.com/25?nameOptions=cities&separator=space&format=json"
+) as f:
+    cities = json.loads(f.read().decode("utf-8"))
 
 while len(band_names) < 25:
-    band = f'{random.choice(b_prefix)}{random.choice(b_main)}{random.choice(b_type)}'
+    band = f"{random.choice(b_prefix)}{random.choice(b_main)}{random.choice(b_type)}"
     if not band in band_names:
         band_names.append(band)
 
 # make a bunch of bands
 bands = []
-for i in range(0,len(band_names)):
+for i in range(0, len(band_names)):
     n = band_names[i]
-    bands.append({
-        "model": "band.band",
-        "pk":100+i,
-        "fields" : {
-            "name": n,
-            "hometown": random.choice(cities),
-            "creation_date": datetime.datetime.now(),
-            "last_activity": datetime.datetime.now(),
+    bands.append(
+        {
+            "model": "band.band",
+            "pk": 100 + i,
+            "fields": {
+                "name": n,
+                # "hometown": random.choice(cities),
+                "creation_date": datetime.datetime.now(),
+                "last_activity": datetime.datetime.now(),
+            },
         }
-    })
+    )
 
 # now put members in bands
 assocs = []
-c=10
+c = 10
 for p in people:
     # put everybody in the first band
-    assocs.append({
-        "model": "band.assoc", 
-        "pk":c, 
-        "fields": {
-            "band": bands[0]["pk"], 
-            "member": p["pk"], 
-            "status": 1, 
-            "is_admin": False, 
-            "is_multisectional": False, 
-            "is_occasional": False, 
-            "color": 1, 
-            "email_me": True, 
-            "hide_from_schedule": False
+    assocs.append(
+        {
+            "model": "band.assoc",
+            "pk": c,
+            "fields": {
+                "band": bands[0]["pk"],
+                "member": p["pk"],
+                "status": 1,
+                "is_admin": False,
+                "is_multisectional": False,
+                "is_occasional": False,
+                "color": 1,
+                "email_me": True,
+                "hide_from_schedule": False,
+            },
         }
-    })
+    )
 
     # now put in a few others
 
@@ -84,28 +97,36 @@ for p in people:
 
     for bpk in otherbands:
         c += 1
-        assocs.append({
-            "model": "band.assoc", 
-            "pk": c, 
-            "fields": {
-                "band": bpk, 
-                "member": p["pk"], 
-                "status": 1, 
-                "is_admin": False, 
-                "is_multisectional": False, 
-                "is_occasional": False, 
-                "color": 1, 
-                "email_me": True, 
-                "hide_from_schedule": False
+        assocs.append(
+            {
+                "model": "band.assoc",
+                "pk": c,
+                "fields": {
+                    "band": bpk,
+                    "member": p["pk"],
+                    "status": 1,
+                    "is_admin": False,
+                    "is_multisectional": False,
+                    "is_occasional": False,
+                    "color": 1,
+                    "email_me": True,
+                    "hide_from_schedule": False,
+                },
             }
-        })
+        )
 
 
-all=people+bands+assocs
+all = people + bands + assocs
+
 
 def myconverter(o):
     if isinstance(o, datetime.datetime):
-        return o.strftime('%Y-%m-%d')
- 
-print(json.dumps(all, default = myconverter))
-#print(json.dumps(all))
+        return o.strftime("%Y-%m-%d")
+
+
+print(json.dumps(all, default=myconverter))
+test_json = json.dumps(all, default=myconverter)
+with open("fixtures/testdata.json", "w") as outfile:
+    outfile.write(test_json)
+
+# print(json.dumps(all))
