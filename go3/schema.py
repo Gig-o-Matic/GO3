27bc507c42a0f3f285a53fc68c93b9def7efab29
Mark <17260591+MarkKleinhaus@users.noreply.github.com>
2021-03-12 07:59:46 -0500

    first poc for user context to only retrieve bands they have assocs for

--- go3/schema.py
+++ go3/schema.py
@@ -1,14 +1,20 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from band.models import *
-from member.models import *
+from band.models import Band, Assoc
+from member.models import Member
 
 
 class BandType(DjangoObjectType):
     class Meta:
         model = Band
         fields = ("name", "hometown", "creation_date")
+
+
+class AssocType(DjangoObjectType):
+    class Meta:
+        model = Assoc
+        fields = ("band", "member", "status")
 
 
 class MemberType(DjangoObjectType):
@@ -18,13 +24,26 @@ class MemberType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    all_bands = graphene.List(BandType)
-    band_by_name = graphene.Field(BandType, name=graphene.String(required=True))
+    # all_bands = graphene.List(BandType)
+    all_bands = graphene.List(AssocType)
+    band_by_name = graphene.Field(
+        BandType, name=graphene.String(required=True))
     all_members = graphene.List(MemberType)
-    member_by_email = graphene.Field(MemberType, email=graphene.String(required=True))
+    member_by_email = graphene.Field(
+        MemberType, email=graphene.String(required=True))
 
-    def resolve_all_bands(self, root):
-        return Band.objects.all()
+    # def resolve_all_bands(self, root):
+    #     return Band.objects.all()
+
+    def resolve_all_bands(self, info):
+        # get all assocs for the user in context
+        user = info.context.user
+        assocs = Assoc.objects.filter(member=user)
+
+        if not info.context.user.is_authenticated:
+            return Assoc.objects.none()
+        else:
+            return assocs
 
     def resolve_band_by_name(self, root, name):
         try:
