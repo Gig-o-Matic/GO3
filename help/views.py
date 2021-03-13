1db846ed15aea3427b9cc9043f15598028425d65
aaronoppenheimer <aoppenheimer@gmail.com>
2021-03-13 13:29:42 -0500

    added calendar help template and view

--- help/views.py
+++ help/views.py
@@ -1,6 +1,25 @@
+"""
+    This file is part of Gig-o-Matic
+
+    Gig-o-Matic is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from member.models import Member
 from django.shortcuts import render
 from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import BandRequestForm
 from lib import email
 
@@ -19,6 +38,16 @@ def credits(request):
 @login_required
 def changelog(request):
     return render(request, 'help/changelog.html')
+
+class CalfeedView(LoginRequiredMixin, TemplateView):
+    template_name = 'help/calfeed.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        member = Member.objects.get(id=self.kwargs['pk'])
+        context['member'] = member
+        return context
+
 
 def whatis(request):
     return render(request, 'help/whatis.html')
