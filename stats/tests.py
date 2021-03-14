747dd6e75ad90ff19e29ddb9d728dc9fea1dc5f4
aaronoppenheimer <aoppenheimer@gmail.com>
2021-03-14 10:57:08 -0400

    adding tests for number-of-members statistic

--- stats/tests.py
+++ stats/tests.py
@@ -15,4 +15,18 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from django.test import TestCase
+from .models import Metric, Stat, BandMetric
+from .tasks import collect_band_stats
+from gig.tests import GigTestBase
+
+class StatsTest(GigTestBase):
+
+    def test_band_member_stat(self):
+        """ show that when a gig is created, every member has a plan """
+        self.assoc_joe()
+        collect_band_stats()
+
+        m = BandMetric.objects.get(name='Number of Members', band=self.band)
+        self.assertEqual(m.stats.count(),1)
+        self.assertEqual(m.stats.first().value,self.band.confirmed_assocs.count())
 
