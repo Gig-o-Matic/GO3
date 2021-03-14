f03cedda6397c1126d2702c6cd00b1e80b777740
aaronoppenheimer <aoppenheimer@gmail.com>
2021-03-14 09:53:25 -0400

    first work on stats management

---
+++ stats/tasks.py
@@ -0,0 +1,30 @@
+"""
+    This file is part of Gig-o-Matic
+
+    Gig-o-Matic is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from .models import BandMetric, Stat
+from band.models import Band
+
+def collect_band_stats():
+
+    # number of members in each band
+    for b in Band.objects.all():
+        m = BandMetric.objects.filter(name='Number of Members', band=b).first()
+        if m is None:
+            m = BandMetric(name='Number of Members', band=b)
+            m.save()
+        Stat(metric=m, value=b.confirmed_members.count()).save()
+        
