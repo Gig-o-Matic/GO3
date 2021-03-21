87147da045ff772d8afefd8c9acf169d269f6c3b
aaronoppenheimer <aoppenheimer@gmail.com>
2021-03-21 14:44:19 -0400

    prettier password change form

--- member/urls.py
+++ member/urls.py
@@ -21,7 +21,7 @@ from . import helpers
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 
 urlpatterns = [
-    path('', views.DetailView.as_view(), name='index'),
+    path('', views.DetailView.as_view(), name='member-detail'),
     path('<int:pk>/', views.DetailView.as_view(), name='member-detail'),
     path('<int:pk>/update', views.UpdateView.as_view(), name='member-update'),
     path('<int:pk>/preferences/update/', views.PreferencesUpdateView.as_view(), name='member-prefs-update'),
