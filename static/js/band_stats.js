3d5f1034293e44c7ebea2717be32cda1f4a93a9f
aaronoppenheimer <aoppenheimer@gmail.com>
2021-04-09 13:04:27 -0400

    wip displaying charts

---
+++ static/js/band_stats.js
@@ -0,0 +1,26 @@
+
+function initStats(container) {
+    const chart = Highcharts.chart(container, {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+}
