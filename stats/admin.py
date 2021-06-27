faf73c9cad5d71629b14f785044294280d239d1e
aaronoppenheimer <aoppenheimer@gmail.com>
2021-06-27 19:33:15 -0400

    added admin for specific stats

--- stats/admin.py
+++ stats/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import BandMetric
+from .models import BandMetric, Stat
 
 # Register your models here.
 @admin.register(BandMetric)
 class BandMetricAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Stat)
+class StatAdmin(admin.ModelAdmin):
     pass
