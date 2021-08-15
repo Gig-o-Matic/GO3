410802c5946a500acf18fc8dce1918153b9a6988
aaronoppenheimer <aoppenheimer@gmail.com>
2021-08-15 11:52:38 -0400

    added url base to stats page

--- stats/views.py
+++ stats/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .helpers import get_all_gigs_over_time_stats, get_all_stats
 import json
 from datetime import datetime
+from go3.settings import URL_BASE
 
 # Create your views here.
 class AllStatsView(LoginRequiredMixin, TemplateView):
@@ -11,6 +12,7 @@ class AllStatsView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['url_base'] = URL_BASE
         context['the_stats'] = get_all_stats()
 
         # get the gigs over time data
