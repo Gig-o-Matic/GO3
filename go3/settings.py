a6aa2e0ac487682ba7bc0d0d68a895ce3d180846
Ben Klang <bklang@wirehack.net>
2024-02-24 11:32:55 -0500

    Add docker development configuration using MySQL

--- go3/settings.py
+++ go3/settings.py
@@ -130,11 +130,14 @@ WSGI_APPLICATION = "go3.wsgi.application"
 
 DATABASES = {
     # The db() method is an alias for db_url().
-    'default': env.db(default='sqlite:////tmp/my-tmp-sqlite.db')
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": os.path.join(BASE_DIR, "var", "go3.sqlite3"),
-    # }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "gig-o-matic",
+        "HOST": "127.0.0.1",
+        "PORT": 13307,
+        "USER": "gig-o-matic",
+        "PASSWORD": "gig-o-matic",
+    }
 }
 
 AUTH_USER_MODEL = "member.Member"
