34d8c2c27a469c3f4a7eee040b8ce15e5ee7c4f7
Aaron Oppenheimer <aaron@Aarons-Mac-mini.lan>
2024-06-02 13:33:58 -0400

    linting

--- stats/util.py
+++ stats/util.py
@@ -14,6 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+from datetime import date
 
 def dateconverter(o):
     if isinstance(o, date):
