86149255ea9133e583be5d46d28edb3637f56ab7
Aaron Oppenheimer <aaron@Aarons-Mac-mini.lan>
2024-09-02 09:32:12 -0400

    updated stats page

--- stats/views.py
+++ stats/views.py
@@ -15,20 +15,18 @@ class AllStatsView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['url_base'] = URL_BASE
-        context['the_stats'] = [
-            ['emails sent today',get_emails_for_date(datetime.now().date())],
-            ['emails sent yesterday',get_emails_for_date((datetime.now().date())-timedelta(days=1))],
-        ]
+        context['the_stats'] = []
+        context['the_stats'].append(['Active Bands Count', [_get_active_bands().count()]])
+        context['the_stats'].append(['Inactive Bands', [b.name for b in _get_inactive_bands()]])
+        context['the_stats'].append(['Emails Sent Today',[get_emails_for_date(datetime.now().date())]])
+        context['the_stats'].append(['Emails Sent Yesterday',[get_emails_for_date((datetime.now().date())-timedelta(days=1))]])
 
         # get the gigs over time data
         context['gigs_over_time_data'] = json.dumps(get_all_gigs_over_time_stats(), default=dateconverter)
 
         # get the email totals for all bands
         data = get_emails_for_all_bands(10)
-        y = [[x[0].name, x[1],x[2]] for x in data]
-        context['top_email_bands'] = y
-
-        context['active_bands_count'] = _get_active_bands().count()
-        context['inactive_bands'] = _get_inactive_bands()
+        y = [f'{x[0].name} ({x[2]} today, {x[1]} all time)' for x in data]
+        context['the_stats'].append(['Top Emailing Bands',y])
 
         return context
