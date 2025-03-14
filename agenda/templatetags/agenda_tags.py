c9b5c8c6855d2c09a9abe001ebcbcc4a33b7d6cc
Nick Pereira <PresidentNick@users.noreply.github.com>
2025-03-13 22:09:55 -0500

    Remove unused import of Plan model in agenda_tags.py

--- agenda/templatetags/agenda_tags.py
+++ agenda/templatetags/agenda_tags.py
@@ -2,8 +2,6 @@ from django import template
 from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
 
-from gig.models import Plan
-
 register = template.Library()
 
 @register.simple_tag
