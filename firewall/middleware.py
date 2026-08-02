a9ea3346df7b21c380fafb4fc8b026fc533734b9
Aaron Oppenheimer <aaron@aoppenheimer.com>
2026-08-02 09:06:35 -0400

    added security.txt and removed the .well-known prefix from the firewall

--- firewall/middleware.py
+++ firewall/middleware.py
@@ -24,8 +24,7 @@ from python_ipware import IpWare
 
 
 BAD_FILE_EXTENSIONS = ['php','env','tar','sql','xml','zip']
-BAD_FILE_PREFIXES = ['/wp-admin', '/apple-touch-icon', '/wp-includes', 
-                     '/.well-known,']
+BAD_FILE_PREFIXES = ['/wp-admin', '/apple-touch-icon', '/wp-includes']
 PROBATION_LIMIT = 30
 BLACKLIST_LIMIT = 300
 
